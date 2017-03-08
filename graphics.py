14315283c554a2472e2b9407acb34b8e45020de9
arbaaz-abz <arbaaz2410@gmail.com>
2017-03-08 09:25:45 -0500

    FIRST REPOSITORY

---
+++ graphics.py
@@ -0,0 +1,9 @@
+
+    import Tkinter
+    root = Tkinter.Tk()
+    l = Tkinter.Label(root, text="Hello, world!\nTkinter on PocketPC!\nSee http://pythonce.sf.net.")
+    b = Tkinter.Button(root, text='Quit', command=root.destroy)
+    l.pack()
+    b.pack()
+   root.mainloop()
+
